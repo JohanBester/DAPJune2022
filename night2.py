3bd3f6cd8a0b1eaf5530027cdb062d95644069c5
Johan Bester <>
2022-08-10 21:36:58 -0500

    class night2 activities

---
+++ night2.py
@@ -0,0 +1,42 @@
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# txt = "The best class in Saint Louis"
+# print("best" in txt)
+
+thisList = ["apple", "banana", "cherry"]
+
+# print("\nLength of list = ", len(thisList))
+# print("\nList item 1 = ", thisList[1])
+
+# thisList[1] = "blackcurrant"
+# print("\nList new item 1 = ", thisList[1])
+
+# thisList[1:3] = ["blackcurrant", "watermelon"]
+# thisList.append("orange")
+# thisList.insert(1, "orange")
+# print("\nThe next new List = ", thisList)
+
+# tropical = ["mango", "pineapple", "papaya"]
+# thisList.extend(tropical)
+# thisList.remove("banana")
+# thisList.pop(1)
+# thisList.pop()
+# del thisList[0]
+# del thisList
+# thisList.clear()
+
+# thisList.reverse()
+# thisList.sort()
+# thisList.sort(reverse=True)
+
+myList = thisList.copy()
+# print("\nThe next new List = ", thisList, myList)
+
+list1 = ["apple", "banana", "cherry"]
+list2 = ["kiwi", "strawberry", "grape"]
+list3 = list1 + list2
+
+
+print("\nThe new list3 = ", list3)
