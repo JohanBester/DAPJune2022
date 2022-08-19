045bc6aa5ecf11bce92dd0f7cf295c44372dbabf
Johan Bester <>
2022-08-18 20:25:14 -0500

    Night4 updates

---
+++ modules.py
@@ -0,0 +1,14 @@
+import datetime
+
+x = datetime.datetime.now()
+# print(x)
+
+y = datetime.datetime(2022, 8, 15)
+# print(y)
+
+z = datetime.datetime.now()
+# print(z)
+
+w = datetime.datetime.now()
+print(w.year)
+print(w.strftime("%A"))
---
+++ night4.py
@@ -0,0 +1,71 @@
+#  ======== LOOPS
+i = 1
+
+# while i < 6:
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+fruits = ["apple", "banana", "grape"]
+
+# for x in fruits:
+#     print("before --", x)
+#     if x == "banana":
+#         break
+#     print("after --", x)
+
+adj = ["red", "big", "juicy"]
+
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
+
+
+# while i < 6:
+#     print(i)
+#     i += 1
+# else:
+#     print("i is no longer less than 6")
+
+# for x in range(2, 6):
+#     print(x)
+
+# for x in range(2, 6):
+#     print(x)
+
+# for z in range(10):
+#     if z == 5:
+#         break
+#     print(z)
+# else:
+#     print("finally done")
+
+#  ======== Try/Except
+
+# try:
+#     print("Hello")
+# except:
+#     print("An exception occurred")
+# else:
+#     print("Nothing went wrong")
+# finally:
+#     print("Try/Exception finished")
+
+#  ======== FUNCTIONS
+
+
+# def my_message():
+#     print("I'm in drawing class!")
+
+
+# my_message()
+
+
+# def familyName(lName):
+#     print(lName + " Smith")
+
+
+# familyName("Jerry")
+# familyName("Amy")
+# familyName("Chad")
